2dd0ddf2c09ad0974f50e05708c932162e34dc31
Italo <italo.dantas@hotmail.com>
2026-01-15 12:14:49 -0300

    Fix: ajustando workflow do github actions 6

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Habilita a exportação estática */
   output: 'export',
-  
-  /* Substitua pelo nome EXATO do seu repositório no GitHub */
-  basePath: '/Calculadora-de-Imc---Titi-IMC',
-
-  /* Opcional: Garante que imagens e links funcionem com o caminho acima */
-  assetPrefix: '/Calculadora-de-Imc---Titi-IMC/',
-
+  images: {
+    unoptimized: true, // Necessário para o GitHub Pages não quebrar as imagens
+  },
   reactCompiler: true,
 };
 
